8a33988a19aa88a0663f26bc962158911bcdbb8c
haquocdat543 <wwwdatha543@gmail.com>
2023-12-09 06:26:26 +0000

    Add terraform
    Add terraform

---
+++ scripts/monitor.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Prometheus
+sudo useradd \
+    --system \
+    --no-create-home \
+    --shell /bin/false prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.47.1/prometheus-2.47.1.linux-amd64.tar.gz
+tar -xvf prometheus-2.47.1.linux-amd64.tar.gz
+sudo mkdir -p /data /etc/prometheus
+cd prometheus-2.47.1.linux-amd64/
+sudo mv prometheus promtool /usr/local/bin/
+udo mv consoles/ console_libraries/ /etc/prometheus/
+sudo mv prometheus.yml /etc/prometheus/prometheus.yml
+sudo chown -R prometheus:prometheus /etc/prometheus/ /data/
+cd
+rm -rf prometheus-2.47.1.linux-amd64.tar.gz
+prometheus --version
+
+cat << EOF | sudo tee -a /etc/systemd/system/prometheus.service
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/data \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.enable-lifecycle
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+journalctl -u prometheus -f --no-pager
+
+# Node Exporter
+sudo useradd \
+    --system \
+    --no-create-home \
+    --shell /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.6.1.linux-amd64.tar.gz
+sudo mv \
+  node_exporter-1.6.1.linux-amd64/node_exporter \
+  /usr/local/bin/
+rm -rf node_exporter*
+cat << EOF | sudo tee -a /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/node_exporter \
+    --collector.logind
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+journalctl -u node_exporter -f --no-pager
+
+cat << EOF | sudo tee -a /etc/prometheus/prometheus.yml
+  - job_name: node_export
+    static_configs:
+          - targets: ["localhost:9100"]
+EOF
+promtool check config /etc/prometheus/prometheus.yml
+
+
+# Grafana
+sudo apt-get install -y apt-transport-https software-properties-common
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update -y
+sudo apt-get -y install grafana
+sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
+
+
